90d0a80821123d90e7a0fb6cfb550a1ae88fa528
Gabriel Rocha <111612298+Bielzinholl@users.noreply.github.com>
2025-03-31 21:57:20 -0300

    Add files via upload
    A ideia não é ser um site funcional e sim apenas seguir oque tinha criado anteriormente pelo figma!

---
+++ JavaScript/script.js
@@ -0,0 +1,13 @@
+
+  const carousel = document.querySelector('.carousel');
+  const next = document.querySelector('.next');
+  const prev = document.querySelector('.prev');
+
+  next.addEventListener('click', () => {
+    carousel.scrollBy({ left: 220, behavior: 'smooth' });
+  });
+
+  prev.addEventListener('click', () => {
+    carousel.scrollBy({ left: -220, behavior: 'smooth' });
+  });
+
